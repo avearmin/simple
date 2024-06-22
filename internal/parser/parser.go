c44daa1fff6b4de7997158310209ecfc74b765b4
avearmin <arminradoncic1@gmail.com>
2024-06-21 21:57:34 -0400

    Add more some parsing ability to parser
    The ParseProgram() function is still incomplete, but has some logic
    implemented for parsing statements, which currently is only an assign
    statement. An errors field was also added to the parser to keep track
    of errors that arise during parsing.

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/avearmin/simple/internal/ast"
 	"github.com/avearmin/simple/internal/lexer"
 	"github.com/avearmin/simple/internal/token"
@@ -8,6 +10,7 @@ import (
 
 type Parser struct {
 	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
@@ -27,5 +30,75 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	if !p.expectCur(token.LParen) {
+		err := fmt.Sprintf("expected token '(', but got '%s'", p.curToken.Type)
+		p.errors = append(p.errors, err)
+		return nil
+	}
+	p.nextToken()
+
+	switch p.curToken.Type {
+	case token.Assign:
+		stmt, err := p.parseAssignStatement()
+		if err != nil {
+			p.errors = append(p.errors, err.Error())
+			return nil
+		}
+		return stmt
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseAssignStatement() (ast.AssignStatement, error) {
+	stmt := ast.AssignStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.Delimiter) {
+		return ast.AssignStatement{}, fmt.Errorf("expected token 'DELIMITER', but got '%s'", p.peekToken.Type)
+	}
+	p.nextToken()
+
+	if !p.expectPeek(token.Ident) {
+		return ast.AssignStatement{}, fmt.Errorf("expected token 'IDENT', but got '%s'", p.peekToken.Type)
+	}
+	p.nextToken()
+
+	stmt.Name = ast.Atom{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.Delimiter) {
+		return ast.AssignStatement{}, fmt.Errorf("expected token 'DELIMITER', but got '%s'", p.peekToken.Type)
+	}
+	p.nextToken()
+
+	stmt.Value = p.parseExpression() // TODO: ADD THIS FUNCTION
+
+	if !p.expectCur(token.RParen) {
+		return ast.AssignStatement{}, fmt.Errorf("expected token ')', but got '%s'", p.peekToken.Type)
+	}
+
+	return stmt, nil
+}
+
+func (p *Parser) expectCur(tokType token.Type) bool {
+	return tokType == p.curToken.Type
+}
+
+func (p *Parser) expectPeek(tokType token.Type) bool {
+	return tokType == p.peekToken.Type
 }
