7f1f601c5e477b6a9b612e9900c302de25937fed
avearmin <arminradoncic1@gmail.com>
2024-06-30 00:24:49 -0400

    Add Line and Col fields to tokens
    Tokens now track the line and column numbers where they were created.
    This will help with parsing plain text to locate errors.

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -9,61 +9,80 @@ type Lexer struct {
 	pos     int
 	nextPos int
 	char    byte
+	line    int
+	col     int
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
+	l := &Lexer{input: input, nextPos: 1, line: 1}
+
+	if len(input) < 1 {
+		l.char = 0
+	} else {
+		l.char = l.input[0]
+	}
+
 	return l
 }
 
 func (l *Lexer) readChar() {
 	if l.nextPos >= len(l.input) {
 		l.char = 0
-	} else {
-		l.pos = l.nextPos
-		l.nextPos = l.pos + 1
-		l.char = l.input[l.pos]
+		return
 	}
+
+	if l.char == '\n' {
+		l.line++
+		l.col = 0
+	} else {
+		l.col++
+	}
+
+	l.pos = l.nextPos
+	l.nextPos = l.pos + 1
+	l.char = l.input[l.pos]
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	line := l.line
+	col := l.col
+
 	switch l.char {
 	case '(':
-		tok = token.NewFromByte(token.LParen, l.char)
+		tok = token.NewFromByte(token.LParen, l.char, line, col)
 	case ')':
-		tok = token.NewFromByte(token.RParen, l.char)
+		tok = token.NewFromByte(token.RParen, l.char, line, col)
 	case '+':
-		tok = token.NewFromByte(token.Add, l.char)
+		tok = token.NewFromByte(token.Add, l.char, line, col)
 	case '=':
-		tok = token.NewFromByte(token.Reassign, l.char)
+		tok = token.NewFromByte(token.Reassign, l.char, line, col)
 	case ':':
 		if l.input[l.nextPos] == '=' {
 			pos := l.pos
-			l.readChar()
-			l.readChar()
-			op := l.input[pos:l.pos]
-			tok = token.NewFromString(token.Assign, op)
-			return tok
-		}
-	case 0:
-		tok = token.NewFromString(token.EOF, "")
-	default:
-		if isWhitespace(l.char) {
-			delimiter := l.readWhitespaces()
-			tok = token.NewFromString(token.Delimiter, delimiter)
-			return tok
-		}
 
+			l.readChar()
+			l.readChar()
+
+			op := l.input[pos:l.pos]
+			tok = token.NewFromString(token.Assign, op, line, col)
+			return tok
+		}
+	case ' ', '\t', '\n', '\r':
+		delimiter := l.readWhitespaces()
+		tok = token.NewFromString(token.Delimiter, delimiter, line, col)
+		return tok
+	case 0:
+		tok = token.NewFromString(token.EOF, "", line, col)
+	default:
 		ident := l.readIdent()
 		if isIdentInt(ident) {
-			tok = token.NewFromString(token.Int, ident)
+			tok = token.NewFromString(token.Int, ident, line, col)
 		} else if isIdentValid(ident) {
-			tok = token.NewFromString(token.Ident, ident)
+			tok = token.NewFromString(token.Ident, ident, line, col)
 		} else {
-			tok = token.NewFromString(token.Illegal, ident)
+			tok = token.NewFromString(token.Illegal, ident, line, col)
 		}
 		return tok
 	}
--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -1,8 +1,9 @@
 package lexer
 
 import (
-	"github.com/avearmin/simple/internal/token"
 	"testing"
+
+	"github.com/avearmin/simple/internal/token"
 )
 
 func TestIsIdentInt(t *testing.T) {
@@ -62,41 +63,41 @@ func TestParse(t *testing.T) {
 			input: `(:= foo 1)
 (= foo (+ foo 1))`,
 			want: []token.Token{
-				{token.LParen, "("},
-				{token.Assign, ":="},
-				{token.Delimiter, " "},
-				{token.Ident, "foo"},
-				{token.Delimiter, " "},
-				{token.Int, "1"},
-				{token.RParen, ")"},
-				{token.Delimiter, "\n"},
-				{token.LParen, "("},
-				{token.Reassign, "="},
-				{token.Delimiter, " "},
-				{token.Ident, "foo"},
-				{token.Delimiter, " "},
-				{token.LParen, "("},
-				{token.Add, "+"},
-				{token.Delimiter, " "},
-				{token.Ident, "foo"},
-				{token.Delimiter, " "},
-				{token.Int, "1"},
-				{token.RParen, ")"},
-				{token.RParen, ")"},
-				{token.EOF, ""},
+				{token.LParen, "(", 1, 0},
+				{token.Assign, ":=", 1, 1},
+				{token.Delimiter, " ", 1, 3},
+				{token.Ident, "foo", 1, 4},
+				{token.Delimiter, " ", 1, 7},
+				{token.Int, "1", 1, 8},
+				{token.RParen, ")", 1, 9},
+				{token.Delimiter, "\n", 1, 10},
+				{token.LParen, "(", 2, 0},
+				{token.Reassign, "=", 2, 1},
+				{token.Delimiter, " ", 2, 2},
+				{token.Ident, "foo", 2, 3},
+				{token.Delimiter, " ", 2, 6},
+				{token.LParen, "(", 2, 7},
+				{token.Add, "+", 2, 8},
+				{token.Delimiter, " ", 2, 9},
+				{token.Ident, "foo", 2, 10},
+				{token.Delimiter, " ", 2, 13},
+				{token.Int, "1", 2, 14},
+				{token.RParen, ")", 2, 15},
+				{token.RParen, ")", 2, 16},
+				{token.EOF, "", 2, 16},
 			},
 		},
 		"Illegal token": {
 			input: `(:= foo 5555xxxx)`,
 			want: []token.Token{
-				{token.LParen, "("},
-				{token.Assign, ":="},
-				{token.Delimiter, " "},
-				{token.Ident, "foo"},
-				{token.Delimiter, " "},
-				{token.Illegal, "5555xxxx"},
-				{token.RParen, ")"},
-				{token.EOF, ""},
+				{token.LParen, "(", 1, 0},
+				{token.Assign, ":=", 1, 1},
+				{token.Delimiter, " ", 1, 3},
+				{token.Ident, "foo", 1, 4},
+				{token.Delimiter, " ", 1, 7},
+				{token.Illegal, "5555xxxx", 1, 8},
+				{token.RParen, ")", 1, 16},
+				{token.EOF, "", 1, 16},
 			},
 		},
 	}
@@ -116,5 +117,5 @@ func TestParse(t *testing.T) {
 }
 
 func isEqualTokens(tokenOne, tokenTwo token.Token) bool {
-	return (tokenOne.Type == tokenTwo.Type) && (tokenOne.Literal == tokenTwo.Literal)
+	return (tokenOne.Type == tokenTwo.Type) && (tokenOne.Literal == tokenTwo.Literal) && (tokenOne.Line == tokenTwo.Line) && (tokenOne.Col == tokenTwo.Col)
 }
