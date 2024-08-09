d7af3979331cfbc2f8ba1312063da17b9a624ae5
avearmin <arminradoncic1@gmail.com>
2024-08-08 22:15:57 -0400

    Add TokenType() func to node interface

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -4,6 +4,7 @@ import "github.com/avearmin/simple/internal/token"
 
 type Node interface {
 	TokenLiteral() string
+	TokenType() token.Type
 }
 
 type Statement interface {
@@ -36,6 +37,7 @@ type AssignStatement struct {
 
 func (as AssignStatement) statementNode()       {}
 func (as AssignStatement) TokenLiteral() string { return as.Token.Literal }
+func (as AssignStatement) TokenType() token.Type { return as.Token.Type }
 
 type ReassignStatement struct {
 	Token token.Token
@@ -45,6 +47,32 @@ type ReassignStatement struct {
 
 func (rs ReassignStatement) statementNode()       {}
 func (rs ReassignStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs ReassignStatement) TokenType() token.Type { return rs.Token.Type }
+
+
+type ElifBlock struct {
+	Token      token.Token
+	Condition  Expression
+	Statements []Statement
+}
+
+type ElseBlock struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+type ConditionalStatement struct {
+	Token        token.Token
+	IfCondition  Expression
+	IfStatements []Statement
+	ElifBlocks   []ElifBlock
+	ElseBlock    ElseBlock
+}
+
+func (cs ConditionalStatement) statementNode()       {}
+func (cs ConditionalStatement) TokenLiteral() string { return cs.Token.Literal }
+func (cs ConditionalStatement) TokenType() token.Type { return cs.Token.Type }
+
 
 type Atom struct {
 	Token token.Token
@@ -53,6 +81,8 @@ type Atom struct {
 
 func (a Atom) expressionNode()      {}
 func (a Atom) TokenLiteral() string { return a.Token.Literal }
+func (a Atom) TokenType() token.Type { return a.Token.Type }
+
 
 type BinaryExpression struct {
 	Token  token.Token
@@ -62,3 +92,5 @@ type BinaryExpression struct {
 
 func (be BinaryExpression) expressionNode()      {}
 func (be BinaryExpression) TokenLiteral() string { return be.Token.Literal }
+func (be BinaryExpression) TokenType() token.Type { return be.Token.Type }
+
